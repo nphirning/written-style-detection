88fc34aff2d1982d6a473c516257bf90e2be33a7
Sam Masling <sam.masling@gmail.com>
2020-05-29 02:02:06 -0400

    pushes

---
+++ model_data_loader.py
@@ -0,0 +1,46 @@
+import torch
+import torch
+from transformers import BertTokenizer
+
+
+class Dataset(torch.utils.data.Dataset):
+  'Characterizes a dataset for PyTorch'
+  def __init__(self, list_IDs, labels, list):
+        'Initialization'
+        self.labels = labels
+        self.list_IDs = list_IDs
+
+  def get_text(self, id):
+    with open(id, 'r') as f:
+        return f.read()
+
+  def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.list_IDs)
+
+  def __getitem__(self, index):
+        'Generates one sample of data'
+        # Select sample
+        id = self.list_IDs[index]
+        ID = self.list[id]
+        example_a = get_text(ID[0])
+        example_b = get_text(ID[1])
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        encoded_dict_a = tokenizer.encode_plus(
+                        self.get_text(example_a),                      # Sentence to encode.
+                        add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+                        max_length = 128,           # Pad & truncate all sentences.
+                        pad_to_max_length = True,
+                        return_attention_mask = True,   # Construct attn. masks.
+                        return_tensors = 'pt',     # Return pytorch tensors.
+                   )
+        encoded_dict_b = tokenizer.encode_plus(
+                        self.get_text(example_b),                      # Sentence to encode.
+                        add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+                        max_length = 128,           # Pad & truncate all sentences.
+                        pad_to_max_length = True,
+                        return_attention_mask = True,   # Construct attn. masks.
+                        return_tensors = 'pt',     # Return pytorch tensors.
+                   )
+        y = self.labels[ID]
+        return encoded_dict_a, encoded_dict_b, y
--- src/model.py
+++ src/model.py
@@ -2,6 +2,10 @@ import torch
 import torch.nn as nn
 import pickle
 from torch.utils.data import Dataset, DataLoader
+from transformers import BertModel
+import os
+import numpy as np
+
 
 INPUT_SIZE = 1000
 TOKEN_SIZE = 768
@@ -12,69 +16,72 @@ VOCAB_SIZE = 10000
 NUM_EPOCHS = 3
 LEARNING_RATE = 0.1
 
-class LSTMBasedModel(nn.Module):
+
+
+
+class BertBasedBiameseModel(nn.Module):
     def __init__(self, hidden_dim, latent_dim, batch_size):
         self.batch_size = batch_size
         self.hidden_dim = hidden_dim
         self.latent_dim = latent_dim
-        # self.embeddings = nn.Embedding(VOCAB_SIZE, TOKEN_SIZE)
-        self.lstm = nn.LSTM(TOKEN_SIZE, self.hidden_dim)
+        self.bert_layer = BertModel.from_pretrained('bert-base-uncased')
+
         self.out_nn = nn.Sequential(
-            nn.Linear(2 * self.hidden_dim, self.latent_dim), 
+            # nn.Conv1d()
+            nn.Linear(2 * self.hidden_dim, self.latent_dim),
             nn.ReLU(),
             nn.Linear(self.latent_dim, 1)
         )
-
-    def init_hidden(self):
-        return (
-            torch.zeros(1, self.batch_size, self.hidden_dim),
-            torch.zeros(1, self.batch_size, self.hidden_dim)
-        )
-
     def forward(self, x):
         # The input `x` is a Python list with 2 elements, each a tensor of size
         #   (INPUT_LEN, BATCH_SIZE, EMBEDDING_SIZE).
-        
-        # Look up embeddings.
-        x1 = x[0]
-        x2 = x[1]
 
-        # Each LSTM output tensor has shape (INPUT_LEN, BATCH_SIZE, HIDDEN_DIM).
-        lstm_out1, _ = self.lstm(x1)
-        lstm_out2, _ = self.lstm(x2)
+        # Look up embeddings.
+        article_one = x[0]
+        article_two = x[1]
+
+
+        _, out_1 = self.bert_layer(article_one['inputs_ids'], attention_mask = article_one['attention_mask'])
+        _, out_2 = self.bert_layer(article_two['inputs_ids'], attention_mask = article_two['attention_mask'])
 
         # Concatenate the final outputs of each LSTM.
         linear_in = torch.cat((
-            lstm_out1[-1].view(self.batch_size, -1), 
-            lstm_out2[-1].view(self.batch_size, -1)
-        ))
+            out_1.view(self.batch_size, -1),
+            out_2.view(self.batch_size, -1)
+        ) ,1)
 
         # Pass the result through a 2-layer NN.
         return self.out_nn(linear_in).view(-1)
 
 # Create model, loss, and optimizer objects.
-model = LSTMBasedModel(HIDDEN_SIZE, LATENT_DIM, BATCH_SIZE)
+model = BertBasedBiameseModel(HIDDEN_SIZE, LATENT_DIM, BATCH_SIZE)
 loss_func = nn.HingeEmbeddingLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-# Get input datasets.
-def split_x_values(x):
-    return [
-        DataLoader(x_train[:, 0, :, :], batch_size=BATCH_SIZE),
-        DataLoader(x_train[:, 1, :, :], batch_size=BATCH_SIZE)
-    ]
-x_train, y_train, x_dev, y_dev, x_test, y_test = get_data()
-train_loaders = split_x_values(x_train)
-label_loader = DataLoader(y_train, batch_size=BATCH_SIZE)
+params = {'batch_size': 32,
+          'shuffle': True,
+          'num_workers': 6}
+
+x_train, y_train = get_data_train()
+x_test, y_test = get_data_test()
+train_loader = DataLoader()
+
+
+training_set = Dataset(x_train.keys(), y_train, x_train)
+training_generator = torch.utils.data.DataLoader(training_set, **params)
+test_set = Dataset(x_test.keys(), y_test, x_test)
+training_generator = torch.utils.data.DataLoader(test_set, **params)
+
 
 for i in range(NUM_EPOCHS):
     sum_loss = 0.0
 
-    for j, batch in enumerate(train_loaders[0]):
+    for batch_ones, batch_twos, labels in enumerate(training_generator[0]):
 
         # Create batch input
-        first_examples = torch.Tensor(train_loaders[0][j])
-        second_examples = torch.Tensor(train_loaders[1][j])
+        device = torch.device()
+        first_examples = torch.Tensor(batch_ones).to(device)
+        second_examples = torch.Tensor(batch_twos).to(device)
         model_input = [first_examples, second_examples]
 
         # Initialize model.
@@ -82,14 +89,67 @@ for i in range(NUM_EPOCHS):
 
         # Run model and compute loss.
         predictions = model(model_input)
-        loss = loss_func(predictions, label_loader[j])
-        sum_loss += loss.item() 
+        loss = loss_func(predictions, labels)
+        sum_loss += loss.item()
 
         # Update weights via backprop
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    
+
     print("Epoch loss: %s" % sum_loss)
+
+
+def get_data_train():
+    authors_to_files = {}
+    for filename in os.listdir('../data/C50/C50train'):
+        author = os.path.join('../data/C50/C50train', filename)
+        for fname in os.listdir(author):
+            a = os.path.join(author, fname)
+            print(a, author)
+            authors_to_files[author] = authors_to_files.get(author, []) + [a]
+
+    ret = {}
+    labels = []
+    for i in range(10000):
+        if np.rand(1) > 0.6:
+            author = authors_to_files[authors_to_files.keys()[np.random.choice(len(authors_to_files.keys()), 1)]]
+            a,b = np.random.choice(len(author), 2)
+            ret[i] = (author[a], author[b])
+            labels[i] = 1
+        else:
+            aone, atwo = authors_to_files[authors_to_files.keys()[np.random.choice(len(authors_to_files.keys()), 2, replace = False)]]
+            a = np.random.choice(len(aone), 1)
+            b = np.random.choice(len(atwo), 1)
+            ret[i] = (aone[a], atwo[b])
+            labels[i] = -1
+    return ret, labels
+
+
+def get_data_test():
+    authors_to_files = {}
+    for filename in os.listdir('../data/C50/C50test'):
+        author = os.path.join('../data/C50/C50test', filename)
+        for fname in os.listdir(author):
+            a = os.path.join(author, fname)
+            print(a, author)
+            authors_to_files[author] = authors_to_files.get(author, []) + [a]
+
+    ret = {}
+    labels = []
+    for i in range(1000):
+        if np.rand(1) > 0.6:
+            author = authors_to_files[authors_to_files.keys()[np.random.choice(len(authors_to_files.keys()), 1)]]
+            a,b = np.random.choice(len(author), 2)
+            ret[i] = (author[a], author[b])
+            labels[i] = 1
+        else:
+            aone, atwo = authors_to_files[authors_to_files.keys()[np.random.choice(len(authors_to_files.keys()), 2, replace = False)]]
+            a = np.random.choice(len(aone), 1)
+            b = np.random.choice(len(atwo), 1)
+            ret[i] = (aone[a], atwo[b])
+            labels[i] = -1
+
+    return ret, labels
 
 
